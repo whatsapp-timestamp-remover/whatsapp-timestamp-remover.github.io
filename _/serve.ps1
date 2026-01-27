aba6c071901ed0fe2b35a78df339c5ab27790148
Jordi Mas Parramon <xordimas@gmail.com>
2026-01-27 23:27:17 +0100

    Add a parsing algorithm to group by name

--- _/serve.ps1
+++
@@ -1,4 +0,0 @@
-while (1) {
-  cp "C:\Users\xordi\_ Development\whatsapp-timestamp-remover\index.html" C:\xampp\htdocs\whatsapp-timestamp-remover;
-  sleep 1;
-}
--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 // [d/m, h:mm] Nom: //
 const REGEX_TIMESTAMP_I_NOM = /^\[(\d{1,2}\/\d{1,2}),\s*(\d{1,2}:\d{2})\]\s+([^:\n]+):\s*/;
+const REGEX_NOM = /\]\s([^:]+):/;
 
 function text_input(text) {
 
+    // text = parsejarEsborrarClaus(text);
+    text = parsejarAgrupatNoms(text);
 
-
-
-    text = parsejarEsborrarClaus(text);
-    
-
-    // text = text.trim();
     document.getElementById("textarea-sortida").value = text;
-
 }
 
 
@@ -21,4 +17,30 @@ function parsejarEsborrarClaus(text) {
     }).join("\n");
 
     return text;
+}
+
+
+function parsejarAgrupatNoms(text) {
+    let linies = [];
+    let nomPrevi;
+
+    text.split("\n").forEach((l,i) => {
+        if (!l) { linies.push(l); return; }
+
+        let nom = l.match(REGEX_NOM)?.[1].trim();
+        if (!nom) { linies.push(l); return; }
+
+        // Primera línia del nou parlant //
+        if (nomPrevi != nom) {
+            if (i != 0) linies.push("");
+            linies.push(`${nom}:`);
+        }
+        
+        linies.push(l.trim().replace(REGEX_TIMESTAMP_I_NOM, ""));
+        
+        nomPrevi = nom;
+        return;
+    });
+
+    return linies.join("\n");
 }
